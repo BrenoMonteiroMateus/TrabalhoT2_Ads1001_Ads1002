19be53b6bed7f9b4e7cea706c0bebf42be4d1f0d
BrenoMonteiroMateus <brenomonteiromateus@gmail.com>
2026-05-23 23:03:41 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/ads1001/t2/TrabalhoT2ADS1001_1002.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ads1001.t2;
+
+/**
+ *
+ * @author Breno e Bia
+ */
+public class TrabalhoT2ADS1001_1002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
